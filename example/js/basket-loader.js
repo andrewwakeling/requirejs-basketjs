eeacfd3ed6aa6798fa5c5f88ab97115f37d886dc
Peter Denev <peter.denev@gmail.com>
2014-03-31 22:35:41 +0300

    pd: example

---
+++ example/js/basket-loader.js
@@ -0,0 +1,43 @@
+/**
+ * requirejs-basketjs
+ * Author: Andrew Wakeling <andrew.wakeling@gmail.com>
+ * requirejs-basketjs may be freely distributed under the MIT license.
+ *
+ * Load require.js modules via basket.js.
+ */
+
+/**
+ * For more information about this function, see comments in "req.load" in require.js.
+ *
+ * @param {Object} context the require context to find state.
+ * @param {String} moduleName the name of the module.
+ * @param {Object} url the URL to the module.
+ */
+;(function () {
+    var original_loader = requirejs.load;
+    requirejs.load = function (context, moduleName, url) {
+        /**
+         * There is currently no public way to access requirejs's config.
+         * As suggested by James Burke, we can somewhat rely on the semi-private "requirejs.s.contexts._.config" as it has not changed between 1.0 and 2.0.
+         *
+         * Source: https://groups.google.com/forum/#!topic/requirejs/Hf-qNmM0ceI
+         */
+
+        var config = requirejs.s.contexts._.config;
+        if (config.basket && config.basket.excludes && config.basket.excludes.indexOf(moduleName) !== -1) {
+            original_loader(context, moduleName, url);
+        } else {
+            var unique = 1;
+            if(config.basket && config.basket.unique && config.basket.unique.hasOwnProperty(moduleName) ){
+                unique = config.basket.unique[moduleName];
+            }
+            basket.require({ url: url,unique:unique }).then(function () {
+                context.completeLoad(moduleName);
+            }, function (error) {
+                // TODO: Support path fallback.
+                context.onError(error);
+            });
+        }
+    };
+}());
+
---
+++ example/js/require_conf.js
@@ -0,0 +1,15 @@
+require.config({
+    baseUrl: 'js/modules',
+    paths: {       
+        general: '../general',        
+    },
+    basket: {
+        excludes: ['general'],
+        unique:{
+            util: 1 //change the number after edit util.js file (it will reload and recache it)
+        }
+    }
+});
+
+// Load the main app module to start the app
+requirejs(["general"]);
